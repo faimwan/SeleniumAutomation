356104f03705f7acdb60ec7d27bcc8304f853e59
Fai Wan <faimwan@yahoo.com>
2018-12-26 18:42:03 -0600

    WordPress Automation

---
+++ WordPressAutomation/Selenium/Driver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WordPressAutomation
+{
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static string BaseAddress
+        {
+            get { return "http://localhost:21272"; }
+        }
+
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver("C:\\Users\\faiw\\SDrivers");
+            TurnOnwait();
+            LoginPage.GoTo();
+
+            // example for SauceLab
+            // DesiredCapabilities capabilities = DesiredCapabilities.Chrome();
+            // capabilities.setCapabilities(CapabilityType.Platform, "Window 10");
+            // capabilities.setCapabilities(CapabilityType.Version, "");
+            // capabilities.setCapabilities("name", "Wordpress test");
+            // capabilities.setCapabilities("username", "iamatester");
+            // capabilities.setCapabilities("accessKey", "");
+            //
+            // Instance = new RemoteWebDriver(
+            //              new uri("http://ondemand.saucelabs.com:80/wd/hub"), capabilities);
+
+        }
+
+        public static void Close()
+        {
+            Instance.Close();
+        }
+
+        public static void Wait(TimeSpan timeSpan)
+        {
+            Thread.Sleep((int) (timeSpan.TotalSeconds * 1000));
+        }
+
+        public static void Nowait(Action action)
+        {
+            TurnOffwait();
+            action();
+            TurnOnwait();
+        }
+
+        private static void TurnOnwait()
+        {
+            Instance.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+        }
+
+        private static void TurnOffwait()
+        {
+            Instance.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(0));
+        }
+    }
+}
---
+++ WordPressAutomation/Workflows/PostCreator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPressAutomation.Workflows
+{
+    public class PostCreator
+    {
+        public static void CreatePost()
+        {
+            NewPostPage.GoTo();
+
+            PreviousTitle = CreateTitle();
+            PreviousBody = CreateBody();
+
+            NewPostPage.CreatePost(PreviousTitle)
+                .WithBody(PreviousBody)
+                .Publish();
+        }
+        public static string PreviousBody { get; set; }
+
+        public static string PreviousTitle { get; set; }
+
+        private static string CreateTitle()
+        {
+            return CreateRandomString() + ", title";
+        }
+
+        private static string CreateBody()
+        {
+            return CreateRandomString() + ", body";
+        }
+
+        private static string CreateRandomString()
+        {
+            var s = new StringBuilder();
+
+            var random = new Random();
+            var cycles = random.Next(5 + 1);
+
+            for (int i = 0; i < cycles; i++)
+            {
+                s.Append(Words[random.Next(Words.Length)]);
+                s.Append(" ");
+                s.Append(Articles[random.Next(Articles.Length)]);
+                s.Append(" ");
+                s.Append(Words[random.Next(Words.Length)]);
+                s.Append(" ");
+            }
+            return s.ToString();
+        }
+
+        public static string[] Articles = new[]
+        {
+            "the", "an", "and", "a", "of", "to", "it", "as"
+        };
+
+        public static string[] Words = new[]
+        {
+            "dog", "rabbit", "tennis", "basketball", "bat", "song", "music", "chocolate"
+        };
+
+        public static void Initialize()
+        {
+            PreviousTitle = null;
+            PreviousBody = null;
+        }
+
+        public static void Cleanup()
+        {
+            if (CreatedPost)
+                TrashPost();
+        }
+
+        private static void TrashPost()
+        {
+            ListPostsPage.TrashPost(PreviousTitle);
+            Initialize();
+        }
+
+        public static bool CreatedPost
+        {
+            get { return !String.IsNullOrEmpty(PreviousTitle); }
+
+        }
+    }
+}
